0d32d321d0906efb4b25ea8696eb9ada09a59007
Herixth <herixth@gmail.com>
2018-12-12 16:10:58 +0800

    Herixth 2018-12-12

--- letter-combinations-of-a-phone-number.py
+++ letter-combinations-of-a-phone-number.py
@@ -15,19 +15,19 @@ class Solution:
         :type digits: str
         :rtype: List[str]
         """
-        nums = { '2': ['a', 'b', 'c'], 
-                 '3': ['d', 'e', 'f'], 
-                 '4': ['g', 'h', 'i'],
-                 '5': ['j', 'k', 'l'],
-                 '6': ['m', 'n', 'o'],
-                 '7': ['p', 'q', 'r', 's'],
-                 '8': ['t', 'u', 'v'],
-                 '9': ['w', 'x', 'y', 'z']
+        nums = {'2': ['a', 'b', 'c'],
+                '3': ['d', 'e', 'f'],
+                '4': ['g', 'h', 'i'],
+                '5': ['j', 'k', 'l'],
+                '6': ['m', 'n', 'o'],
+                '7': ['p', 'q', 'r', 's'],
+                '8': ['t', 'u', 'v'],
+                '9': ['w', 'x', 'y', 'z']
                 }
         lens = len(digits)
         ToList = []
         self.dfs(ToList, "", 0, lens, digits, nums)
-        return ToList        
+        return ToList
 
 
 if __name__ == '__main__':
